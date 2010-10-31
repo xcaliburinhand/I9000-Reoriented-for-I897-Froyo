2e506fb24eb8eea2e6af08bbb9566c418c8ad77d
xcaliburinhand <github@muteheadlight.net>
2010-10-31 16:47:09 -0400

    bln_v2.0

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -2120,6 +2120,13 @@ static void smdkc110_power_off(void)
 	while (1) ;
 }
 
+/* <begin> added by mr */
+// not the best solution, but a working one.
+#ifdef CONFIG_KEYPAD_CYPRESS_TOUCH_USE_BLN
+extern bool BacklightNotification_enabled;
+#endif
+/* <end> added by mr */
+
 void s3c_config_sleep_gpio_table(int array_size, unsigned int (*gpio_table)[3])
 {
 	u32 i, gpio;
@@ -2130,6 +2137,16 @@ void s3c_config_sleep_gpio_table(int array_size, unsigned int (*gpio_table)[3])
 		s3c_gpio_slp_cfgpin(gpio, gpio_table[i][1]);
 		s3c_gpio_slp_setpull_updown(gpio, gpio_table[i][2]);
 	}
+
+/* <begin> added by mr */
+#ifdef CONFIG_KEYPAD_CYPRESS_TOUCH_USE_BLN
+	if(BacklightNotification_enabled)
+	{
+	    s3c_gpio_slp_cfgpin(_3_GPIO_TOUCH_EN, S3C_GPIO_SLP_OUT1);
+	    s3c_gpio_slp_setpull_updown(_3_GPIO_TOUCH_EN, S3C_GPIO_PULL_DOWN);
+	}
+#endif
+/* <end> added by mr */
 
 	if (gpio_get_value(GPIO_PS_ON))
 	{
