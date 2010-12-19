731f3cd4f203e4ebec15013d36951b3f2b8c73f3
xcaliburinhand <github@muteheadlight.net>
2010-12-18 20:47:54 -0500

    3e recovery from captivate

--- usr/initrd_files/voodoo/scripts/recovery_wrapper.sh
+++ usr/initrd_files/voodoo/scripts/recovery_wrapper.sh
@@ -5,13 +5,14 @@ if test -f /voodoo/run/cwm_enabled; then
 	echo "starting CWM recovery"
 	/voodoo/scripts/cwm_start.sh&
 else
-	if test -x /system/bin/recovery; then
-		# Froyo standard recovery 3e
-		echo "starting Froyo recovery"
-		/system/bin/recovery&
-	else
+#	if test -x /system/bin/recovery; then
+#		# Froyo standard recovery 3e
+#		echo "starting Froyo recovery"
+#		/system/bin/recovery&
+#
+#	else
 		# Eclair standard recovery 2e
 		echo "starting Eclair recovery"
 		/sbin/recovery&
-	fi
+#	fi
 fi
